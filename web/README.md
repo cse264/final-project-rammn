690880b00cf5c8ddcc404bd9ed89fca45d1dedd6
Maximillian Machado <maximillian.k.machado@gmail.com>
2022-04-04 01:00:09 -0400

    vue.js is now able to be served from flask

---
+++ web/README.md
@@ -0,0 +1,29 @@
+# Frontend Manual
+
+## Requirements:
+
+- npm
+- ...
+
+## Run server:
+
+### Local Deploy:
+1. 
+
+### Heroku Deploy:
+1. 
+
+## Installation Instructions:
+
+1. 
+
+## Run tests:
+
+1. 
+
+## Environment Configuration:
+
+
+## TODO:
+
+- get database url from the path
---
+++ web/vueapp/vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  assetsDir: '../static',
+  publicPath: '',
+  publicPath: undefined,
+  outputDir: path.resolve(__dirname, '../../backend/RAMMN/templates'),
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+};
