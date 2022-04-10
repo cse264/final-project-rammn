3dc7aceeb799fa6c045bd135fb3cfbed91d3f43d
Maximillian Machado <maximillian.k.machado@gmail.com>
2022-04-10 18:47:59 -0400

    added database example code

--- backend/RAMMN/db.py
+++ backend/RAMMN/db.py
@@ -1,8 +1,34 @@
+from lib2to3.pytree import _Results
+from unittest import result
 import psycopg2
+
+import sys
 
 import click
 from flask import current_app, g
-from flask.cli import with_appcontext
+from flask.cli import with_appcontext            
+
+def get_db():
+    if 'db' not in g:
+        try:
+            g.db = psycopg2.connect(current_app.config['DATABASE_URL'])
+        except psycopg2.DatabaseError as e:
+            print(e, file=sys.stderr)
+            raise e
+
+    return g.db
+
+def example_method():
+    """This function is an example on how to execute queries with cursor"""
+    db = get_db()
+
+    query = "SELECT * FROM tb1"
+
+    with db.cursor() as cursor:
+        cursor.execute(query)
+        records = [row for row in cursor.fetchall()]
+        cursor.close()
+        return records
 
 def init_db():
     db = get_db()
@@ -19,12 +45,6 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
-
-def get_db():
-    if 'db' not in g:
-        g.db = psycopg2.connect(current_app.config['DATABASE_URL'])
-
-    return g.db
 
 
 def close_db(e=None):
