64d85bbf5747395cb6c8cb2a0966813ead363f0f
Alexander Carr <jalexandercarr@gmail.com>
2022-04-19 12:08:43 -0400

    Installed template for front end

--- web/vueapp/src/main.js
+++ web/vueapp/src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+import CoreuiVue from '@coreui/vue'
+import CIcon from '@coreui/icons-vue'
+import { iconsSet as icons } from '@/assets/icons'
+import DocsCallout from '@/components/DocsCallout'
+import DocsExample from '@/components/DocsExample'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(CoreuiVue)
+app.provide('icons', icons)
+app.component('CIcon', CIcon)
+app.component('DocsCallout', DocsCallout)
+app.component('DocsExample', DocsExample)
+
+app.mount('#app')
